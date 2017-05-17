6d4dd0b80590aabf026cd2acb439db843a6e5ccc
vvthang <thangvubk@gmail.com>
2017-05-17 14:46:25 +0700

    add lesson2

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const express = require('express');
 // setup express app
 const app = express();
 
+// initialize routes
+app.use('/api',require('./routes/api'));
+
 // listen for requests
 app.listen(process.env.port || 8888, function(){
 	console.log('Listening for requests...')
 });
+
 
